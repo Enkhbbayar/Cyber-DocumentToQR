f67698b59952c8ae0d232798ecaa5bc21104020f
Enkhbbayar <154262169+Enkhbbayar@users.noreply.github.com>
2024-05-15 16:12:20 +0800

    commit

---
+++ src/components/Header1.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    // <ul className="flex p-3 justify-between items-center bg-slate-900 w-screen fixed z-50">
+      <li className="flex-1 margin-2"><b>QR ТАМГАЛАГЧ</b></li>
+      // {/* <li>
+      //   <button>
+      //     <a href="#main">Эхлэх</a>
+      //   </button>
+      // </li> */}
+    
+  );
+};
+
+export default Header;
